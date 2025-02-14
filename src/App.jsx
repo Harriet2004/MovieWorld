d40a85cbcdd61fa70817775a0ce72386efb2dcbf
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-14 14:46:10 +0400

    Added useStates for buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const Card = ({title}) => {
-  return <div className = 'card'>
+
+  const [loved, setLoved] = useState(false);
+
+  return ( 
+    <div className = 'card'>
     <h2>{title}</h2>
+    
+    <button onClick = {() => setLoved(!loved)}>
+      {loved ? "❤️" : "🤍"}
+    </button>
+    
     </div>
+  )
 }
 
 const App = () => {
+
   return (
     <div className = 'card-contain'>
-    <Card title = "Star wars" />
-    <Card title = "Gumball" />
-    <Card title = "Australia" />
+    <Card title = "Die with a smile" />
+    <Card title = "APT" />
+    <Card title = "Old Town Road" />
     </div>
   )
 }
