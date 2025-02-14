9528fe12beb6a7b084580a95fcd47d1bef6757a4
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-14 17:45:44 +0400

    Added the movies API

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './components/search'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
   const[search, setSearch] = useState('');
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 const Search = ({search, setSearch}) => {
   return (
     <div className = "search">
-        <img src = './search.svg' />
+        <div>
+        <img src = 'search.svg' alt = "search" />
         <input 
             type='text'
             placeholder='Search a movie'
             value={search}
             onChange={(event) => setSearch(event.target.value)}
         />
+        </div>
     </div>
   )
 }
