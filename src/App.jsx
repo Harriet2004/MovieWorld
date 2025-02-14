be15d3997bb0b0e4f40d8b24643ae72c4a01e256
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-14 15:01:37 +0400

    Applied useEffects to the titles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 const Card = ({title}) => {
-
+  const [count, setCount] = useState(0);
   const [loved, setLoved] = useState(false);
 
+  useEffect(() => {
+    console.log(`${title} has been liked: ${loved}`)
+  }, [loved]);
+
   return ( 
-    <div className = 'card'>
-    <h2>{title}</h2>
+    <div className = 'card' onClick = {() => setCount((prevState) => prevState + 1)}>
+    <h2>{title} <br /> {count ? count : null}</h2>
     
     <button onClick = {() => setLoved(!loved)}>
       {loved ? "❤️" : "🤍"}
