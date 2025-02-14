a3b1e1b59433ee733c68663e862d710050cb7fff
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-14 21:26:46 +0400

    Minor changes in appwrite.js

--- src/appwrite.js
+++ src/appwrite.js
@@ -10,26 +10,26 @@ const client = new Client()
 
 const database = new Databases(client);
 
-export const searchCount = async (search, movie) => {
+export const searchCount = async (searchTerm, movie) => {
     try {
-        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [Query.equal('search', search)])
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [Query.equal('searchTerm', searchTerm)])
         if (result.documents.length > 0) {
             const doc = result.documents[0]
             await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {count: doc.count + 1})
         } else {
-            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {search, count: 1, movie_id : movie.id, poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`})
+            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {searchTerm, count: 1, movie_id : movie.id, poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`})
         }
     } catch (error) {
         console.log(error);
     }
 }
 
-export const trendingMovies = async () => {
+export const getTrendingMovies = async () => {
     try {
-        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [Query.limit(5), Query.orderDesc('count')])
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [Query.limit(5), Query.orderDesc('count')]);
         return result.documents;
     } catch(error) {
-        console.log(error);
+        console.error(error);
     }
     
 }
