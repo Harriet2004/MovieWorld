8650c3325819fe9d549fe260616f5ecb4a22e3b6
Harriet2004 <s3933558@student.rmit.edu.au>
2025-02-14 23:57:27 +0400

    Replaced the search logo

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ const Search = ({search, setSearch}) => {
   return (
     <div className = "search">
         <div>
-        <img src = 'search.svg' alt = "search" />
+        <img src = 'glass.png' alt = "search" />
         <input 
             type='text'
             placeholder='Search a movie'
